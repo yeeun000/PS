9a3a36ee381124cc491d28e5d0e31eab1980b093
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2022-09-21 10:52:23 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 72 ms, Memory: 11100 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    static StringBuilder sb;
+
+    public static void main(String[] args){
+        input();
+        // logic();
+    }
+    static void input(){
+        sb = new StringBuilder();
+        sb.append("고려대학교");
+        System.out.println(sb);
+    }
+}
