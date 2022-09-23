3ac1dd98be62cf7f124859055c2e773619b21eee
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2022-09-24 01:54:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12780 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        int N=str.charAt(0);
+        System.out.println(N);
+    }
+}
