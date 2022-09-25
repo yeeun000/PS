4de431512a6a70a70477eccc7329699252d002c4
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2022-09-25 16:15:17 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 628 ms, Memory: 59508 KB -BaekjoonHub

--- 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.java
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.java
@@ -1,13 +1,12 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static StringTokenizer st;
     static StringBuilder sb;
     static BufferedReader br;
+    static StringTokenizer st;
     static int N, M;
-    static int[] A;
-    static int[] D;
+    static int[] arr, arr2;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -15,48 +14,69 @@ public class Main {
     }
 
     static void input() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
+        br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        A = new int[N];
-        D = new int[M];
+
+        arr = new int[N];
+        arr2 = new int[M];
+
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
         }
+
         for (int i = 0; i < M; i++) {
-            D[i] = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
+            arr2[i] = num;
         }
     }
 
     static void logic() {
-        Arrays.sort(A);
-        int X;
+        Arrays.sort(arr);
+
         for (int i = 0; i < M; i++) {
-            X = BinarySearch(A, 0, N - 1, D[i]);
-            sb.append(X).append('\n');
+            // 중복 값일 때 처리
+
+            int lower_temp = lower_Bound(arr, 0, N, arr2[i]);
+            int br_temp = brSearch(arr, 0, N - 1, arr2[i]);
+
+            if (lower_temp < br_temp) {
+                sb.append(lower_temp).append('\n');
+            } else {
+                sb.append(br_temp).append('\n');
+            }
+
         }
-        System.out.println(sb);
+
+        System.out.print(sb);
     }
 
-    static int BinarySearch(int[] arr, int L, int R, int X) {
+    static int brSearch(int[] arr, int L, int R, int X) {
         while (L <= R) {
             int mid = (L + R) / 2;
-            if (arr[mid] == X){
-                if(mid!=0&&arr[mid]==arr[mid-1]) {
-                    for (int i = 0; i < N; i++) {
-                        if (arr[mid] == arr[i])
-                            return i;
-                    }
-                }
+            if (arr[mid] == X) {
                 return mid;
-            }
-            else if (arr[mid] < X)
+            } else if (arr[mid] < X) {
                 L = mid + 1;
-            else
+            } else {
                 R = mid - 1;
+            }
         }
         return -1;
+    }
+
+    static int lower_Bound(int[] arr, int L, int R, int X) {
+        while (L < R) {
+            int mid = (L + R) / 2;
+            if (arr[mid] >= X) {
+                R = mid;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return L;
     }
 }
