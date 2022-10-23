779dc0769def3d9f51860cc307c3a45f7e392de6
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2022-10-23 11:10:30 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 428 ms, Memory: 18124 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Main {
+    static BufferedReader br;
+    static int N;
+    public static void main(String[]args)throws IOException{
+        br=new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(br.readLine());
+        for(int i=N;i>0;i--){
+            for(int j=i-1;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int s=(2*N)-(2*i-1);s>0;s--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<N;i++){
+            for(int j=i;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int s=(2*N-1)-(2*i);s>0;s--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
