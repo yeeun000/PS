e494b529d041c2d61d5807749f01646e7479b3c0
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2022-12-16 11:27:18 +0900

    [Bronze IV] Title: Easy-to-Solve Expressions, Time: 96 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int []arr=new int[3];
+        for(int i=0;i<3;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        if(arr[0]+arr[1]==arr[2])
+            sb.append("1");
+        else if(arr[0]*arr[1]==arr[2])
+            sb.append("2");
+        else sb.append("3");
+        System.out.println(sb);
+    }
+}
