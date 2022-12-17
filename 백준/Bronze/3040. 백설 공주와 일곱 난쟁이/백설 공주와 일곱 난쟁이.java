80c63371949055a03b5f34c4ea78f114dd7bd174
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2022-12-17 23:00:02 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 72 ms, Memory: 11468 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,63 +1,51 @@
 import java.io.*;
-import java.util.*;
-
 public class Main {
-    static BufferedReader br;
-    static StringBuilder sb;
-    static int[] arr;
-
-    public static void main(String[] args) throws IOException {
-        input();
-        logic();
-    }
-
-    static void input() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        arr = new int[9];
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+    public static void main(String[] args)throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        int arr[]=new int [9];
+        for(int i=0;i<9;i++){
+            arr[i]=Integer.parseInt(br.readLine());
         }
-    }
 
-    static void logic() {
-        Arrays.sort(arr);
-        int sum=0;
-        for (int a = 0; a < 3; a++) {
-            for (int b = a+1; b < 4; b++) {
-                for (int c = b+1; c < 5; c++) {
-                    for (int d = c+1; d < 6; d++) {
-                        for (int f = d+1; f < 7; f++) {
-                            for (int e = f+1; e < 8; e++) {
-                                for (int g = e+1; g < 9; g++) {
-                                    sum=arr[a]+arr[b]+arr[c]+arr[d]+arr[f]+arr[e]+arr[g];
-                                    if(sum==100) {
-                                        sb.append(arr[a]).append('\n');
-                                        sb.append(arr[b]).append('\n');
-                                        sb.append(arr[c]).append('\n');
-                                        sb.append(arr[d]).append('\n');
-                                        sb.append(arr[f]).append('\n');
-                                        sb.append(arr[e]).append('\n');
-                                        sb.append(arr[g]);
+        int s=0;
+
+        for(int a=0;a<3;a++){
+            for(int b=a+1;b<4;b++){
+                for(int c=b+1;c<5;c++){
+                    for(int d=c+1;d<6;d++){
+                        for(int e=d+1;e<7;e++){
+                            for(int f=e+1;f<8;f++){
+                                for(int g=f+1;g<9;g++){
+                                    s=arr[a]+arr[b]+arr[c]+arr[d]+arr[e]+arr[f]+arr[g];
+                                    if(s==100){
+                                        sb.append(arr[a]+"\n"+arr[b]+"\n"+arr[c]+"\n"+arr[d]+"\n"+arr[e]+"\n"+arr[f]+"\n"+arr[g]);
                                         break;
                                     }
                                 }
-                                if(sum==100)
+                                if(s==100){
                                     break;
-                            }if(sum==100)
+                                }
+                            }
+                            if(s==100){
                                 break;
+                            }
                         }
-                        if(sum==100)
+                        if(s==100){
                             break;
+                        }
                     }
-                    if(sum==100)
+                    if(s==100){
                         break;
+                    }
                 }
-                if(sum==100)
+                if(s==100){
                     break;
+                }
             }
-            if(sum==100)
+            if(s==100) {
                 break;
+            }
         }
         System.out.println(sb);
     }
