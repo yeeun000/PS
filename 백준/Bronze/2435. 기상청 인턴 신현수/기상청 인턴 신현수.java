b50870e388c51a2797708285d4448876be65deff
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2022-12-28 21:50:18 +0900

    [Bronze I] Title: 기상청 인턴 신현수, Time: 76 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/2435. 기상청 인턴 신현수/기상청 인턴 신현수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int K=Integer.parseInt(st.nextToken());
+        st=new StringTokenizer(br.readLine());
+        int []arr=new int[N];
+        int []s=new int [N-K+1];
+        int p=0;
+        for(int i=0;i<K;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+            s[p]+=arr[i];
+        }
+        int q=0;
+        for(int i=K;i<N;i++){
+            p++;
+            arr[i]=Integer.parseInt(st.nextToken());
+            s[p]=s[p-1]+arr[i]-arr[q];
+            q++;
+        }
+
+        Arrays.sort(s);
+        sb.append(s[N-K]);
+        System.out.println(sb);
+    }
+}
