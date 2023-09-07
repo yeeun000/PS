7f9a37f85dde54145777917750e4aeac5d146e84
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2023-09-07 16:38:34 +0900

    [Bronze IV] Title: Tri-du, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+        if(a>b)
+            sb.append(a);
+        else sb.append(b);
+        System.out.println(sb);
+    }
+}
