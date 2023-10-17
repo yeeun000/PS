ac8fd03cce6c7fdd8b692ec099f83865d3a6aada
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2023-10-17 10:36:48 +0900

    [Bronze I] Title: 소인수분해, Time: 168 ms, Memory: 11704 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main {
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        int N=Integer.parseInt(br.readLine());
+        int i=2;
+        while(N!=1){
+            if(N%i==0) {
+                N = N / i;
+                sb.append(i).append("\n");
+            }
+            else i++;
+        }
+        System.out.println(sb);
+    }
+}
