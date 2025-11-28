540abac026ccb206dfcbabb37e3c9cc560a929c0
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2025-11-28 11:11:57 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.38 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        my_string=my_string.toLowerCase();
+        char[] ch=my_string.toCharArray();
+        Arrays.sort(ch);
+        answer=new String(ch);
+        return answer;
+    }
+}
