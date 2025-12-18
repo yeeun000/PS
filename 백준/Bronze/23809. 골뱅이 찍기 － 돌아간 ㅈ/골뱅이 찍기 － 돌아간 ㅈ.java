5a0e30e611c229b8ac5dfaea537b0e5ec521fb97
yeeun000 <108613541+yeeun000@users.noreply.github.com>
2025-12-18 22:33:19 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 164 ms, Memory: 18388 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.java
@@ -0,0 +1,67 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        int N=Integer.parseInt(br.readLine());
+        for(int i=0;i<1*N;i++){
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            for(int j=0;j<3*N;j++){
+                sb.append(" ");
+            }
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+        for(int i=0;i<1*N;i++){
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            for(int j=0;j<2*N;j++){
+                sb.append(" ");
+            }
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+        for(int i=0;i<1*N;i++){
+            for(int j=0;j<3*N;j++){
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+        for(int i=0;i<1*N;i++){
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            for(int j=0;j<2*N;j++){
+                sb.append(" ");
+            }
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+        for(int i=0;i<1*N;i++){
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            for(int j=0;j<3*N;j++){
+                sb.append(" ");
+            }
+            for(int j=0;j<1*N;j++){
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
